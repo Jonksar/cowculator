4f2291e5a0035164b59591064177e04fc3fa054c
Kevin Deldycke <kevin@deldycke.com>
2017-02-14 09:42:49 +0200

    refactor plotting && started making example code for DynamicPlot. Renamed DataPlot to DynamicPlotNumpy

--- realtimeplot/RTPlot.py
+++ realtimeplot/RTPlot.py
@@ -13,6 +13,7 @@ class DynamicPlotter():
         self._interval = int(sampleinterval*1000)
         self._bufsize = int(timewindow/sampleinterval)
         self.databuffer = collections.deque([0.0]*self._bufsize, self._bufsize)
+        self.givedata_buffer = None
         self.x = np.linspace(-timewindow, 0.0, self._bufsize)
         self.y = np.zeros(self._bufsize, dtype=np.float)
 
@@ -38,8 +39,18 @@ class DynamicPlotter():
         new = 10.*math.sin(time.time()*frequency*2*math.pi) + noise
         return new
 
+    def givedata(self, data):
+        self.givedata_buffer = data
+
     def updateplot(self):
-        self.databuffer.append( self.getdata() )
+        # If we haven't received data to givedata_buffer
+        if self.givedata_buffer == None:
+
+            # Get your data from self.getdata()
+            self.databuffer.append( self.getdata() )
+        else:
+            self.databuffer.append( self.givedata_buffer )
+
         self.y[:] = self.databuffer
         self.curve.setData(self.x, self.y)
         self.app.processEvents()
@@ -47,6 +58,37 @@ class DynamicPlotter():
     def run(self):
         self.app.exec_()
 
-# Use this to see it in action
-# x = DynamicPlotter()
-# x.run()
+    def data_wrapper(self, func, *args, **kwargs):
+        def wrapped(*args, **kwargs):
+            while True:
+                res = func(*args, **kwargs)
+                time.sleep(float(self._interval) / 1000)
+                self.givedata(res)
+
+        return wrapped
+
+
+if __name__ == '__main__':
+    # Import threading for having the thread to generate data
+    import threading
+
+    # Use this to see it in action
+    plt = DynamicPlotter()
+
+    # Example of how to give data to DynamicPlotter
+    @plt.data_wrapper
+    def data_gen(frequency=0.5, max_noise=1.):
+        noise = random.normalvariate(0., max_noise)
+        new = 10.*math.sin(time.time()*frequency*2*math.pi) + noise
+
+        return new
+
+    # Define the thread
+    th = threading.Thread(target=data_gen,
+                          kwargs={'frequency': 0.5,
+                                  'max_noise': 1.})
+    th.daemon = True
+
+    # Finally when the set-up is ready, start everything
+    th.start() # Start thread
+    plt.run()  # Start plotting
