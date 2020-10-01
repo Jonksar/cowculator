e6e755c3493faa918ea4b38493ebd3bdefe1c269
Daniel Melendrez <dmelendrez@gmail.com>
2020-10-01 01:00:26 +0100

    Update the compatibility with Python 3.x
    __Quasi-atomic__, namely __minor__ changes on the print( ) function and since __raw_input__ is deprecated I changed it to __input__
    
    Thanks for your examples. Found them in SOF and they've been of great help
    
    Cheers 🍺

--- examples/plotting_example.py
+++ examples/plotting_example.py
@@ -55,9 +55,9 @@ def numpy_sin_plot():
     pass
 
 if __name__ == '__main__':
-    print "This is the plotting example of KoalaTools toolkit.\nEnter a number in order to see that particular example.\n\t(1) Fast real time plotting of sin + noise\n\t(2) Slower real time plotting of multiple lines sin + noise\n"
+    print ("This is the plotting example of KoalaTools toolkit.\nEnter a number in order to see that particular example.\n\t(1) Fast real time plotting of sin + noise\n\t(2) Slower real time plotting of multiple lines sin + noise\n")
 
-    choice = raw_input("Enter your choice:")
+    choice = input("Enter your choice:")
 
     if "1" in choice:
         plot_1d()
